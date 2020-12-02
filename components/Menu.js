aed4ef4b308f509dcb0f1571efa48340be22d92f
Jamie <jamielynjenks@gmail.com>
2020-12-02 14:41:09 -0700

    MVP complete, stretch in progrss

--- components/Menu.js
+++ components/Menu.js
@@ -17,17 +17,39 @@ let menuItems = [
       {each menu item as an <li>}
     </ul>
   </div>
-
-  The 'menuMaker' takes an array of menu items as its only argument.
-
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
-
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
-
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
-
-  Step 5: Don't forget to return your div.menu.
-
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+function menuMaker(items){
+  let menu = document.createElement('div');
+  let button = document.querySelector('.menu-button');
+
+  items.forEach(li=>{
+    let item = document.createElement('li');
+    item.textContent = li;
+    menu.appendChild(item)
+  })
+
+  menu.classList.add('menu');
+
+  button.addEventListener('click', function(){
+    menu.classList.toggle('menu--open');
+  })
+
+  return menu;
+}
+
+let menu = menuMaker(menuItems);
+console.log(menu)
+document.querySelector('.header').appendChild(menu);
+//   The 'menuMaker' takes an array of menu items as its only argument.
+
+//   Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+//   Add those items to the <ul>
+
+//   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+
+//   Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+
+//   Step 5: Don't forget to return your div.menu.
+
+//   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+// */
